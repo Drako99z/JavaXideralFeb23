98977490f0be479752acb2c8ced237ff61fbf72b
Miguel Rugerio <mrugerio@gmail.com>
2023-02-27 17:58:07 -0600

    Demo de como enciar a una jsp

--- DemoWeb/src/main/java/com/curso/HolaMundoServlet.java
+++ DemoWeb/src/main/java/com/curso/HolaMundoServlet.java
@@ -1,6 +1,8 @@
 package com.curso;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,8 @@ public class HolaMundoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served Xideral at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/HolaMundo.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
