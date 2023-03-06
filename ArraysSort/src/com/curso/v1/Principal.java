2b4cfc9fff43f1f323bf9735c0a0463f15d6cc68
Miguel Rugerio <mrugerio@gmail.com>
2023-03-06 09:51:38 -0600

    Sort Arrays

---
+++ ArraysSort/src/com/curso/v1/Principal.java
@@ -0,0 +1,24 @@
+package com.curso.v1;
+import java.util.Arrays;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Empleado[] arrayEmpleados = {
+				new Empleado("Patrobas", 20, 567.89),
+				new Empleado("Tercio", 18, 167.89),
+				new Empleado("Andronico", 30, 267.89),
+				new Empleado("Epeneto", 22, 967.89)				
+		};
+		
+		Arrays.sort(arrayEmpleados);
+		
+		for (Empleado e: arrayEmpleados) {
+			System.out.println(e);
+		}
+
+		
+	}
+
+}
