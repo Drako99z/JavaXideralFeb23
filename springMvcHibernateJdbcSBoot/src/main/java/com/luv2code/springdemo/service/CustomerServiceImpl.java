09c162f1b790291804b12c100eae42808fafd675
Miguel Rugerio <mrugerio@gmail.com>
2023-03-08 13:31:14 -0600

    Se añadio el @Qualifier

--- springMvcHibernateJdbcSBoot/src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ springMvcHibernateJdbcSBoot/src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import com.luv2code.springdemo.entity.Customer;
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
+	@Qualifier("customerDAOJdbc")
 	private CustomerDAO customerDAO;
 	
 	@Override
