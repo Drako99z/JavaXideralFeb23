ab2239d3c3e941b128e040109ace86df2731c810
Miguel Rugerio <mrugerio@gmail.com>
2023-03-14 15:47:22 -0600

    Servicio Rest con manejo de Excepciones

---
+++ springBootRestJpaException/src/main/java/com/luv2code/springdemo/controller/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo.controller;
+
+public class CustomerNotFoundException extends RuntimeException {
+	
+	   public CustomerNotFoundException(String message) {
+	        super(message);
+	    }
+
+}
